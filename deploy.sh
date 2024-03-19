cbb251ded639039db085b030b3925c3178a5d6eb
naisuuu <cjmosawan@yahoo.ie>
2024-03-19 18:24:16 +0000

    pm2 kill

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
 # Start the application with the process name sample_app using pm2
+pm2 kill
 pm2 start ./bin/www --name SampleApp
 pm2 monitor SampleApp
-pm2 flush
-pm2 logs
+
